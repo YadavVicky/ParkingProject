ed9c23367fd7d8d0f095ead1cfb7f552e9901d33
YadavVicky <50552164+YadavVicky@users.noreply.github.com>
2021-10-05 22:00:37 +0530

    Removing map cred

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,3 @@
-// mapboxgl.accessToken = 'pk.eyJ1IjoidmluYXkxOTk5IiwiYSI6ImNrZ3ZwOGp1bzAyaWYzMXJyeTExNDVhZnUifQ.4cEakcbGU7-PNGfJ27mi-Q';
-// const map = new mapboxgl.Map({
-// container: 'map', // container ID
-// style: 'mapbox://styles/mapbox/dark-v10', // style URL
-// center: city[0].geometry.coordinates, // starting position [lng, lat]
-// zoom: 9 // starting zoom
-// });
-
-// for(var i = 0; i < city[0].check.length; i++) {
-//     const marker = new mapboxgl.Marker()
-//     .setLngLat(city[0].check[i].geometry.coordinates)
-//     .addTo(map);
-//   }
-
-mapboxgl.accessToken = 'pk.eyJ1IjoidmluYXkxOTk5IiwiYSI6ImNrZ3ZwOGp1bzAyaWYzMXJyeTExNDVhZnUifQ.4cEakcbGU7-PNGfJ27mi-Q';
-
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
